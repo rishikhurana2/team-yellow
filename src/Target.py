cd7ff7b9814398581e983e33737d048e82ce8d6a
RobertYL <rob3rtyl33@gmail.com>
2017-11-18 07:00:42 -0800

    Fixed Thresholding and Processor

--- src/Target.py
+++ src/Target.py
@@ -2,27 +2,12 @@ import cv2
 import math
 
 class Target:
-	def __init__(self, contour):
-		# Initialize array of size 4x2 to contain 4 points with (x,y) tuples
-		points = [[0 for y in range(4)] for x in range(2)]
-
-		# Draw a rotated bounding box that minimizes area outside of rectangle		
-		rect = cv2.boundingRect(contour)
+	def __init__(self, approx):
+		points = [0 for y in xrange(4)]
 		
-		# May be errors here, Rishi please check if it works
-		#		
-		# Take out the 4 points of the rectangle
-		corners = cv2.boxPoints(rect)
+		for x in xrange(4):
+			points[x] = approx[x]
 		
-		# Place all 8 points into the initialized array
-		count = 0
-		for point in corners:
-			points[count][0] = point.pt[0]
-			points[count][1] = point.py[1]
-		#
-		#
-		#
-
 		# Find which ones are "pairs"
 		xValue = []
 		yValue = []
@@ -41,28 +26,29 @@ class Target:
 				if points[yValue[x]][1] < points[y][1]:
 					yValue.insert(y, x)
 
-		center = [0, 0]
-		width = 0
-		height = 0
+		self.center = [0, 0]
+		self.width = 0
+		self.height = 0
+		
 		# H = 0, S = 1, V = 2
-		targetType = -1
+		self.targetType = -1
 		for x in range(4):
-			center[0] += points[x][0]
-			center[1] += points[x][1]
+			self.center[0] += points[x][0]
+			self.center[1] += points[x][1]
 
 			if x == 3:
-				center[0] /= 4
-				center[1] /= 4
+				self.center[0] /= 4
+				self.center[1] /= 4
 
-		width = int(math.sqrt(((points[yValue[0]][1] - points[yValue[1]][1])**2) + ((points[yValue[0]][0] - points[yValue[1]][0])**2)))
-		height = int(math.sqrt(((points[xValue[0]][1] - points[xValue[1]][1])**2) + ((points[xValue[0]][0] - points[xValue[1]][0])**2)))
+		self.width = int(math.sqrt(((points[yValue[0]][1] - points[yValue[1]][1])**2) + ((points[yValue[0]][0] - points[yValue[1]][0])**2)))
+		self.height = int(math.sqrt(((points[xValue[0]][1] - points[xValue[1]][1])**2) + ((points[xValue[0]][0] - points[xValue[1]][0])**2)))
 
 		if width > height and abs([yValue[0]][1] - points[yValue[1]][1]) < width * 0.1:
-			targetType = 0
+			self.targetType = 0
 		if height > width and abs([xValue[0]][0] - points[xValue[1]][0]) < height * 0.1:
-			targetType = 2
+			self.targetType = 2
 		else:
-			targetType = 3
+			self.targetType = 3
 
 	def getType(self):
 		return self.targetType
--- src/TargetDetector.py
+++ src/TargetDetector.py
@@ -3,29 +3,34 @@ import numpy as np
 from VideoDevice import VideoDevice
 
 class TargetDetector:
-	def TargetDetect(self, image):
-		global hsv_img
-		global Cimg
-		Cimg = image
-		hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-		cv2.imshow("HSV Image", hsv_img)
+	def threshold(self, image):
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		#cv2.imshow("HSV Image", hsv_img)
+		
 		THRESHOLD_MIN = np.array([100,0,100],np.uint8)
 		THRESHOLD_MAX = np.array([255,255,255],np.uint8)
-		frame_threshed = cv2.inRange(hsv_img, THRESHOLD_MIN, THRESHOLD_MAX)
-		cv2.imshow("Threshed", frame_threshed)
+		
+		image = cv2.inRange(image, THRESHOLD_MIN, THRESHOLD_MAX)
+		
+		return image
+		
+	def contour(self, image)
+		self.found = False
+		#cv2.imshow("Threshed", frame_threshed)
 		count = -1
-		global images
-		global contours
-		global hierarchy
-		images, contours, hierarchy = cv2.findContours(frame_threshed,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-		cv2.drawContours(Cimg,contours,count,(255,255,255),2)
-		cv2.imshow("Contour Image", Cimg)
-	def getFound(self):
-		global contours
-		if contours == True:
-			return True
-		else:
-			return false  #is this right?
-# TargetDetect() = inputs the camera frame and processes it, should output a single contour and if there aren't any contours make the "found" boolean 0
-# getFound() = if a target is found, 0 or 1 output
 
+		images, contours, hierarchy = cv2.findContours(frame_threshed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		#cv2.drawContours(Cimg, contours, count, (255,255,255), 2)
+		#cv2.imshow("Contour Image", Cimg)
+		
+		for cont in contours:
+			count += 1
+			
+			approx = cv2.approxPolyDP(cont, 0.01 * cv2.arcLength(cont, True), True)
+			area = cv2.contourArea(approx)
+			if len(approx) == 4 and area > 300:
+				self.found = True
+				return approx
+		
+	def getFound(self):
+			return self.found
