a5f9150975a315427aa722a9fb3987f6dcc417e5
RobertYL <rob3rtyl33@gmail.com>
2017-12-02 00:10:51 -0800

    Fixed Target and Target Processor

--- src/Target.py
+++ src/Target.py
@@ -3,13 +3,8 @@ import math
 
 class Target:
 	def __init__(self, approx):
-		minX = temp[0][0][0]
-        maxX = temp[0][0][0]
-        minY = temp[0][0][1]
-        maxY = temp[0][0][1]
-		
-		upperPoints = [[0 for y in range(2)] for x in range(0)]
-		rightPoints = [[0 for y in range(2)] for x in range(0)]
+		upperPoints = [[0 for y in range(2)] for x in range(2)]
+		rightPoints = [[0 for y in range(2)] for x in range(2)]
 		
 		# H = 0, S = 1, V = 2
 		self.targetType = -1
@@ -32,7 +27,7 @@ class Target:
 				rightPoints[1][0] = point[0][0]
 				rightPoints[1][1] = point[0][1]
 				
-			elif point[0][1] > upperPoints[0][1]:
+			if point[0][1] > upperPoints[0][1]:
 				upperPoints[1][0] = upperPoints[0][0]
 				upperPoints[1][1] = upperPoints[0][1]
 
@@ -46,16 +41,15 @@ class Target:
 		self.center[0] /= 4
 		self.center[1] /= 4
 		
-		
 		self.width = int(math.sqrt(((upperPoints[0][0] - upperPoints[1][0])**2) + ((upperPoints[0][1] - upperPoints[1][1])**2)))
 		self.height = int(math.sqrt(((rightPoints[0][0] - rightPoints[1][0])**2) + ((rightPoints[0][1] - rightPoints[1][1])**2)))
 
-		if width > height and abs(upperPoints[0][1] - upperPoints[1][1]) < self.width * 0.1:
+		if self.width > self.height and abs(upperPoints[0][1] - upperPoints[1][1]) < self.width * 0.1:
 			self.targetType = 0
-		if height > width and abs(rightPoints[0][0] - rightPoints[1][0]) < self.height * 0.1:
+		elif self.height > self.width and abs(rightPoints[0][0] - rightPoints[1][0]) < self.height * 0.1:
 			self.targetType = 2
 		else:
-			self.targetType = 3
+			self.targetType = 1
 
 	def getType(self):
 		return self.targetType
