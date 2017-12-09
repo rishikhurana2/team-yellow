f9c7c7964785ee616b71bbe0f64b53eecc056f52
RobertYL <rob3rtyl33@gmail.com>
2017-12-09 13:56:39 -0800

    Hotfix: AppConfig tabs

--- src/AppConfig.py
+++ src/AppConfig.py
@@ -14,72 +14,72 @@ class AppConfig:
 
 
 	def getDeviceID(self):
-	'''Returns the camera ID'''
+		'''Returns the camera ID'''
 	
 		return self.deviceID
 	
 	
 	def getIsFile(self):
-	'''Returns the image name'''
+		'''Returns the image name'''
 	
 		return self.isFile
 
 
 	def getIsDevice(self):
-    '''Returns if there is a camera'''
-    
+		'''Returns if there is a camera'''
+
 		return self.isDevice
 
 
 	def getIsHeadless(self):
-	'''Returns if running as headless'''
+		'''Returns if running as headless'''
 	
 		return self.isHeadless
 
 
 	def getIsNetworking(self):
-	'''Returns if running with networking'''
+		'''Returns if running with networking'''
 	
 		return self.isNetworking
 
 
 	def getIsDebug(self):
-	'''Returns if running debug mode'''
+		'''Returns if running debug mode'''
 	
 		return self.isDebug
 
 
 	def setDeviceID(self, num):
-	'''Changes the camera ID'''
+		'''Changes the camera ID'''
 	
 		self.deviceID = num
 
 
 	def setIsFile(self, num):
-	'''Changes the image name'''
+		'''Changes the image name'''
 	
 		self.isFile = num
 
 
 	def setIsDevice(self, num):
-	'''Changes if there is a camera or not'''
+		'''Changes if there is a camera or not'''
 	
 		self.isDevice = num
 
 
 	def setIsHeadless(self, num):
-	'''Changes if running as headless or not'''
+		'''Changes if running as headless or not'''
 	
 		self.isHeadless = num
 
 
 	def setIsNetworking(self, num):
-	'''Changes if running with networking or not'''
+		'''Changes if running with networking or not'''
 	
 		self.isNetworking = num
 
 
 	def setIsDebug(self, num):
-	'''Changes if running on debug mode or not'''
+		'''Changes if running on debug mode or not'''
 	
 		self.isDebug = num
